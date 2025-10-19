673e010a24145106bce0a5a1713fd0537defa343
MizeMau <johannes.zielenbach@outlook.de>
2025-10-19 12:38:27 +0200

    Init projekt

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using LiveCharts;
+using LiveCharts.Definitions.Charts;
+using LiveCharts.Definitions.Series;
+using LiveCharts.Wpf;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Aes
+{
+    public partial class MainWindow : Window
+    {
+        private readonly DispatcherTimer _timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new ViewModel();
+            this.pieChart.DataHover += PieChart_DataHover;
+
+            _timer.Interval = TimeSpan.FromMilliseconds(50);
+            _timer.Tick += UpdateProgress;
+            _timer.Start();
+        }
+        private void PieChart_DataHover(object sender, ChartPoint chartPoint)
+        {
+            var pieSeries = chartPoint.SeriesView as PieSeries;
+            this.myCustomTooltip.Value = $"{chartPoint.Y}€ ({chartPoint.Participation:P0})";
+            this.myCustomTooltip.Fill = pieSeries!.Fill;
+        }
+        private void UpdateProgress(object? sender, EventArgs e)
+        {
+            progressBar.Value += 2;
+            if (progressBar.Value >= progressBar.Maximum)
+                progressBar.Value = progressBar.Minimum;
+        }
+    }
+
+    public class ViewModel
+    {
+        public SeriesCollection SeriesCollection { get; set; }
+
+
+        public ViewModel()
+        {
+            SeriesCollection = new SeriesCollection
+            {
+                new PieSeries { Title = "Mary",  Values = new ChartValues<double> { 10 }, Fill = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFFFFF")!},
+                new PieSeries { Title = "John",  Values = new ChartValues<double> { 20 }},
+                new PieSeries { Title = "Alice", Values = new ChartValues<double> { 30 }},
+                new PieSeries { Title = "Bob",   Values = new ChartValues<double> { 40 }},
+                new PieSeries { Title = "Charlie", Values = new ChartValues<double> { 50 }},
+            };
+        }
+    }
+}
---
+++ ToolTip.xaml.cs
@@ -0,0 +1,52 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Aes
+{
+    public partial class ToolTip : UserControl, IChartTooltip
+    {
+        public ToolTip()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public TooltipData Data { get; set; }
+
+        private string _value { get; set; }
+        public string Value
+        {
+            get => _value;
+            set { _value = value; OnPropertyChanged(nameof(Value)); }
+        }
+
+        private Brush _fill { get; set; }
+        public Brush Fill
+        {
+            get => _fill;
+            set { _fill = value; OnPropertyChanged(nameof(Fill)); }
+        }
+
+        public TooltipSelectionMode? SelectionMode { get; set; }
+
+        public void Show() => Visibility = Visibility.Visible;
+
+        public void Hide() => Visibility = Visibility.Hidden;
+
+        public void Move(Point point)
+        {
+            Canvas.SetLeft(this, point.X);
+            Canvas.SetTop(this, point.Y);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
